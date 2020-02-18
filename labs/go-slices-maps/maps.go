d7f9953caef1944d72ffda561aee44bdea8b34d5
Sebastian Woolfolk <swoolfolkmar@gmail.com>
2020-02-17 20:59:26 -0600

    Solved
    Signed-off-by: Sebastian Woolfolk <swoolfolkmar@gmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
 
+func WordCount(s string) map[string]int {
+
+	dic := make(map[string]int)
+	for _,w := range strings.Fields(s){
+		dic[w]++
+	}
+
+	return dic
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,33 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+
+
+	v := make([][]uint8, dy)
+
+	for i := 0; i < len(v); i++ {
+
+		v[i] = make([]uint8,dx)
+
+	}
+
+	for i := 0; i < len(v); i++ {
+
+		for j := 0; j < len(v[i]); j++ {
+
+			if i < j {
+				v[i][j] = 125
+			}	else{
+				v[i][j] = 255
+			}
+		}
+	}
+
+	return v
+}
+
 func main() {
+	pic.Show(Pic)
 }
